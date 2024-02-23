11122386dcf4544f9ca1a31d973e98c5cb805eb9
Kas <kasghobadi@gmail.com>
2024-02-23 16:55:39 -0500

    Enum for trade types task

--- Assets/Scripts/TradeOffer.cs
+++ Assets/Scripts/TradeOffer.cs
@@ -1,7 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum TradeType
+{
+    ATTACKER,
+    REPAIR,
+    SMUGGLER,
+    MERCHANT,
+    //add more as needed to classify your trade types easily 
+}
 /// <summary>
 /// When we spawn a character on the space TV -- they all come with a trade offer. 
 /// There are positive and negative affects of each trade offer. 
@@ -10,6 +17,7 @@ using UnityEngine;
 /// </summary>
 public class TradeOffer : MonoBehaviour
 {
+    public TradeType tradeType;
     //what are the data values this trade uses as effects?
     //Could be positive or negative numbers. 
     //Include inverse setting - when we deny a trade, the inverse effect happens. 
